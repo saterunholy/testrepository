7f57f4ac654b2e4e83a0e6a7d49866109c7bec04
Jose Moreno <58490017+saterunholy@users.noreply.github.com>
2020-10-09 10:31:23 -0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,24 @@
 export const funcParams = 'data, theme, echartsInstance, echarts';
 
 const funcBody = `const series = data.series.map((s) => {
-  const sData = s.fields.find((f) => f.type === 'number').values.buffer;
-  const sTime = s.fields.find((f) => f.type === 'time').values.buffer;
-
+  const sData = s.fields.find((f) => f.name === 'value').values.buffer;
+  const sTime = s.fields.find((f) => f.name === 'pelicula').values.buffer;
+  
   return {
+    sData , sTime 
   };
 });
 
-const axisOption = {
+const test = series[0].sData.map((item,i) => {
+
+  return {
+  value:  series[0].sData[i], 
+  name: series[0].sTime[i], 
+  itemStyle: {color: '#AA4FAA', borderColor: '#000000', shadowBlur: 200}
+   }}
+  );
+
+  const axisOption = {
   axisTick: {
     show: false,
   },
@@ -59,104 +69,19 @@ return {
     containLabel: true,
   },
   series: [
-      {
-            name: 'Results 1',
+    { name: 'piecharttemp1',
             type: 'pie',
-            selectedMode: 'single',
-            radius: [0, '30%'],
-
+            radius: [30, 110],
+            center: ['50%', '50%'],
+            roseType: 'area',
             label: {
-                position: 'inner'
+                position: 'inner',
+                fontWeight: 'bold'
             },
-            labelLine: {
-                show: false
-            },
-            data: [
-                {value: 335, name: 'Data 1', selected: true},
-                {value: 679, name: 'Data 2'},
-                {value: 1548, name: 'Data 3'},
-                {value: 2000, name: 'Data 4'}
-              
-            ],
-        itemStyle: {
-                color: '#CB4482',
-                borderColor: '#000000',
-                borderWidth: '0.3',
-                shadowBlur: 200,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-            },
-        },
-    
-        {
-            name: 'Results 2',
-            type: 'pie',
-            radius: ['40%', '55%'],
-            label: {
-                formatter: '{a|{a}}{abg|}\\n{hr|}\\n  {b|{b}：}{c|{c}}  {per|{d}%}  ',
-                backgroundColor: '#eee',
-                borderColor: '#aaa',
-                borderWidth: 1,
-                borderRadius: 10,
-                 shadowBlur:3,
-                 shadowOffsetX: 2,
-                 shadowOffsetY: 2,
-                 shadowColor: '#999',
-                 padding: [0, 7],
-                rich: {
-                    a: {
-                        color: '#195292',
-                        lineHeight: 17,
-                        align: 'center'
-                    },
-                    // abg: {
-                    //     backgroundColor: '#333',
-                    //     width: '100%',
-                    //     align: 'right',
-                    //     height: 22,
-                    //     borderRadius: [4, 4, 0, 0]
-                    // },
-                    hr: {
-                        borderColor: '#195292',
-                        width: '100%',
-                        borderWidth: 0.5,
-                        height: 0
-                    },
-                    b: {
-                        fontSize: 16,
-                        color: '#44C5CB',
-                        lineHeight: 22
-                    },
-                    c: {
-                      color: "#44C5CB"
-                    },
-                    per: {
-                        color: '#eee',
-                        backgroundColor: '#44C5CB',
-                        padding: [2, 4],
-                        borderRadius: 2
-                    }
-                }
-            },
-            data: [
-                {value: 335, name: 'Data 1'},
-                {value: 310, name: 'Data 2'},
-                {value: 234, name: 'Data 3'},
-                {value: 135, name: 'Data 4'},
-                {value: 1048, name: 'Data 5'},
-                {value: 251, name: 'Data 6'},
-                {value: 147, name: 'Data 7'},
-                {value: 102, name: 'Data 8'}
-            ],
-            itemStyle: {
-                color: '#44C5CB',
-                borderColor: '#243EC5',
-                borderWidth: '0.3',
-                shadowBlur: 200,
-                shadowColor: 'rgba(100, 100, 0, 10)'
-            },
-        }
+            data: test           
+    },
   ],
-  graphic: [
+   graphic: [
     {
             type: 'image',
             id: 'logo',
@@ -169,8 +94,8 @@ return {
                 opacity: 1
             }
   },
-    ]
-};`;
+     ],
+};
 
 // const getOption = `function (${funcParams}) {
 //   ${funcBody}
